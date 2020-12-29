f585361e15d4b680cd7e5452113c86722dab6175
Robert Gieseke <rob.g@web.de>
2020-12-29 17:15:21 +0100

    Add vaccination data
    Data from Excel file dated 28.12.2020, 16:15h

---
+++ scripts/vaccinations.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import pandas as pd
+
+# https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.html
+
+root = Path(__file__).parents[1]
+
+excel_file = root / "Impfquotenmonitoring.xlsx"
+
+
+meta = """Digitales Impfquotenmonitoring zur COVID-19-Impfung
+Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.htm
+
+"""
+
+notes = pd.read_excel(excel_file, sheet_name=0, index_col=0)
+meta += "\n".join(
+    [str(i) for i in notes.dropna(how="all", axis=1).index.values if not pd.isnull(i)]
+)
+meta += "\n"
+
+
+df = pd.read_excel(excel_file, sheet_name=1, usecols="A:G", nrows=17, index_col=0)
+df = df.astype("Int64")
+df = df.rename(columns={"Pflegeheim-bewohnerIn": "PflegeheimbewohnerIn"})
+
+footnotes = pd.read_excel(excel_file, sheet_name=1, skiprows=18, index_col=0)
+
+meta += "\n".join(
+    [
+        str(i)
+        for i in footnotes.dropna(how="all", axis=1).index.values
+        if not pd.isnull(i)
+    ]
+)
+meta += "\n\n"
+
+with open(root / "data/vaccinations/vaccinations.csv", "w") as f:
+    for line in meta.splitlines():
+        f.write(f"# {line}\n")
+    f.write(df.to_csv())
