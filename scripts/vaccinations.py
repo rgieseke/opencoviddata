2d3f99f353389c74c6b402e0c852af74ca640ec2
Robert Gieseke <rob.g@web.de>
2020-12-30 15:22:58 +0100

    Unify handling of metadata

--- scripts/vaccinations.py
+++ scripts/vaccinations.py
@@ -9,17 +9,11 @@ root = Path(__file__).parents[1]
 excel_file = root / "raw/Impfquotenmonitoring.xlsx"
 
 
-meta = """Digitales Impfquotenmonitoring zur COVID-19-Impfung
-Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.htm
-
-"""
+meta = ["Digitales Impfquotenmonitoring zur COVID-19-Impfung",
+"Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.htm", ""]
 
 notes = pd.read_excel(excel_file, sheet_name=0, index_col=0)
-meta += "\n".join(
-    [str(i) for i in notes.dropna(how="all", axis=1).index.values if not pd.isnull(i)]
-)
-meta += "\n"
-
+meta += [str(i) for i in notes.dropna(how="all", axis=1).index.values if not pd.isnull(i)]
 
 df = pd.read_excel(excel_file, sheet_name=1, usecols="A:G", nrows=17, index_col=0)
 df = df.astype("Int64")
@@ -27,16 +21,14 @@ df = df.rename(columns={"Pflegeheim-bewohnerIn": "PflegeheimbewohnerIn"})
 
 footnotes = pd.read_excel(excel_file, sheet_name=1, skiprows=18, index_col=0)
 
-meta += "\n".join(
-    [
+meta += [
         str(i)
         for i in footnotes.dropna(how="all", axis=1).index.values
         if not pd.isnull(i)
     ]
-)
-meta += "\n\n"
+meta += [""]
 
 with open(root / "data/vaccinations/vaccinations.csv", "w") as f:
-    for line in meta.splitlines():
+    for line in meta:
         f.write(f"# {line}\n")
     f.write(df.to_csv())
