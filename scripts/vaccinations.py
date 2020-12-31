a4dddf0d1fb8c6475ff36992edc4c8b5fb6ba000
Robert Gieseke <rob.g@web.de>
2020-12-31 13:28:29 +0100

    Update vaccination data
    Better handling of multi-column metadata.

--- scripts/vaccinations.py
+++ scripts/vaccinations.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import pandas as pd
+from openpyxl import load_workbook
 
 # https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.html
 
@@ -9,23 +10,38 @@ root = Path(__file__).parents[1]
 excel_file = root / "raw/Impfquotenmonitoring.xlsx"
 
 
-meta = ["Digitales Impfquotenmonitoring zur COVID-19-Impfung",
-"Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.htm", ""]
+meta = [
+    "Digitales Impfquotenmonitoring zur COVID-19-Impfung",
+    "Quelle: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquoten-Tab.htm",
+    "",
+]
 
-notes = pd.read_excel(excel_file, sheet_name=0, index_col=0)
-meta += [str(i) for i in notes.dropna(how="all", axis=1).index.values if not pd.isnull(i)]
+wb = load_workbook(excel_file)
+meta_sheet = wb[wb.sheetnames[0]]
+
+
+def iter_rows(ws):
+    for row in ws.iter_rows():
+        yield [
+            cell.value
+            for cell in row
+            if (not pd.isnull(cell.value)) and (not cell.value == "")
+        ]
+
+
+notes = list(iter_rows(meta_sheet))
+for row in notes:
+    if len(row) > 0:
+        meta += [" ".join(row)]
 
 df = pd.read_excel(excel_file, sheet_name=1, usecols="A:G", nrows=17, index_col=0)
 df = df.astype("Int64")
 df = df.rename(columns={"Pflegeheim-bewohnerIn": "PflegeheimbewohnerIn"})
 
-footnotes = pd.read_excel(excel_file, sheet_name=1, skiprows=18, index_col=0)
-
-meta += [
-        str(i)
-        for i in footnotes.dropna(how="all", axis=1).index.values
-        if not pd.isnull(i)
-    ]
+footnotes = wb[wb.sheetnames[1]]
+for row in list(iter_rows(footnotes))[18:]:
+    if len(row) > 0:
+        meta += [" ".join(row)]
 
 # Remove duplicates in metadata
 meta = list(dict.fromkeys(meta))
